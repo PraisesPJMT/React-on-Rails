da701c652e80ca69d36f91c64980b398482d9ad2
PraisesPJMT <praisesmusa@outlook.com>
2023-01-09 16:14:00 +0100

    Setup webpack dependencise

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
-const path    = require("path")
-const webpack = require("webpack")
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: "./app/javascript/application.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
   },
   output: {
     filename: "[name].js",
@@ -14,7 +23,7 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+      maxChunks: 1,
+    }),
+  ],
+};
