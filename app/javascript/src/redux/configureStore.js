32651492a5bb0cd365ef63899b0bbf1cf2794b8b
PraisesPJMT <praisesmusa@outlook.com>
2023-01-10 17:41:26 +0100

    Reconfigure React store

--- app/javascript/src/redux/configureStore.js
+++ app/javascript/src/redux/configureStore.js
@@ -1,13 +1,16 @@
-import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
-import messagesReducer from "./messages/messagesSlicer";
+import messageReducer, { getMessage } from "./messages/messages";
+
+const reducer = combineReducers({
+    messageReducer,
+});
 
 // Redux store
-const store = configureStore(
-  {
-    reducer: messagesReducer,
-  },
-  applyMiddleware(thunk)
-);
+const store = configureStore({
+    reducer,
+    middleware: [thunk],
+});
 
+store.dispatch(getMessage());
 export default store;
